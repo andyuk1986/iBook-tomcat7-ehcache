ad835c494ef919962ee1c7cc34d693dfc577e1d7
anna.manukyan <anna.mnkyn@gmail.com>
2012-08-20 11:27:25 +0200

    Test comit.

--- src/main/java/iBook/utils/Utils.java
+++ src/main/java/iBook/utils/Utils.java
@@ -101,7 +101,7 @@ public final class Utils {
     public Session openTransaction() {
         Session session = getSession();
         session.beginTransaction();
-
+       System.out.println("test commit");
         return session;
     }
 
